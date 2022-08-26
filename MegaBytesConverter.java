51db930b8e7c5482c15ae922e56cee48f802c849
VIGNESH <111691971+RM-VIGNESH@users.noreply.github.com>
2022-08-26 21:15:18 +0530

    Update MegaBytesConverter.java

--- MegaBytesConverter.java
+++ MegaBytesConverter.java
@@ -1,10 +1,11 @@
 public class MegaBytesConverter {
     public static void printMegaBytesAndKiloBytes(int KiloBytes){
+        // KB cant be 0 or less than 0....
         if (KiloBytes < 0){
             System.out.println("Invalid Value");
         }else{
-            int mega= KiloBytes/1024;
-            int bytes= KiloBytes % 1024;
+            int mega= KiloBytes/1024; // 1MB = 1024KB
+            int bytes= KiloBytes % 1024; 
 
             System.out.println(KiloBytes + " KB = " + mega + " MB and " + bytes +" KB" );
         }
