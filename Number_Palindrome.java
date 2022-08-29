0cffbc53f6db802f6c88cfb2bc15ddebc23892ee
VIGNESH <111691971+RM-VIGNESH@users.noreply.github.com>
2022-08-29 22:44:53 +0530

    Update Number_Palindrome.java

--- Number_Palindrome.java
+++ Number_Palindrome.java
@@ -1,7 +1,7 @@
 package academy.learnprogramming;
 
 public class Number_Palindrome {
-
+    // driver code
     public static void main(String[] args){
         System.out.println(isPalindrome(1221));    }
 
